c8ad691de041856ea374363861a51b6bae126276
Nick Jenkins <nickjenkinsweb@gmail.com>
2016-12-16 13:59:42 -0800

    Added ability to remove code depending on the target environment using rollup replace plugin

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ let gulp = require('gulp');
 let uglify = require('gulp-uglify');
 let rollup = require('rollup-stream');
 let babel = require('rollup-plugin-babel');
+let replace = require('rollup-plugin-replace');
 let babelRegister = require('babel-register');
 let rename = require('gulp-rename');
 let streamify = require('gulp-streamify');
@@ -13,7 +14,7 @@ let source = require('vinyl-source-stream');
 
 let pkg = require('./package.json');
 
-gulp.task('build_libs', function () {
+gulp.task('build_lib', function () {
   return rollup({
     entry: './src/index.js',
     format: 'umd',
@@ -22,11 +23,32 @@ gulp.task('build_libs', function () {
     plugins: [
       babel({
         exclude: 'node_modules/**'
+      }),
+      replace({
+        exclude: 'node_modules/**',
+        __DEV__: true
       })
     ]
   }).pipe(source(pkg.name + '.js'))
-    .pipe(gulp.dest('./lib'))
-    .pipe(rename(pkg.name + '.min.js'))
+    .pipe(gulp.dest('./lib'));
+});
+
+gulp.task('build_min_lib', function () {
+  return rollup({
+    entry: './src/index.js',
+    format: 'umd',
+    exports: 'named',
+    moduleName: 'bristle',
+    plugins: [
+      babel({
+        exclude: 'node_modules/**'
+      }),
+      replace({
+        exclude: 'node_modules/**',
+        __DEV__: false
+      })
+    ]
+  }).pipe(source(pkg.name + '.min.js'))
     .pipe(streamify(uglify()))
     .pipe(gulp.dest('./lib'));
 });
@@ -41,4 +63,4 @@ gulp.task('watch', function () {
   gulp.watch('./spec/*.js', ['spec']);
 });
 
-gulp.task('default', ['build_libs'], function () {});
+gulp.task('default', ['build_lib', 'build_min_lib'], function () {});
