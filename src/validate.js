5e5cee7402cca69fefd2b53a6ee6c47a409401a3
Nick Jenkins <nickjenkinsweb@gmail.com>
2017-01-06 17:28:12 -0800

    Fixed bug in y value check

--- src/validate.js
+++ src/validate.js
@@ -1,6 +1,6 @@
 export let validatePoint = (p) => {
   if(!Array.isArray(p)) return "value must be an array";
   if(typeof p[0] !== 'number' || Number.isNaN(p[0])) return "The X ([0]) component must be a number";
-  if(typeof p[0] !== 'number' || Number.isNaN(p[0])) return "The Y ([1]) component must be a number";
+  if(typeof p[1] !== 'number' || Number.isNaN(p[1])) return "The Y ([1]) component must be a number";
   return undefined;
 };
