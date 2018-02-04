75aeaa37312e32722a5a43db1e7ee9a7401c3a65
Nick Jenkins <nickjenkinsweb@gmail.com>
2018-02-04 15:11:54 -0800

    Remove projection process from renderToCanvas

--- src/renderToCanvas.js
+++ src/renderToCanvas.js
@@ -1,22 +1,20 @@
 import instructionCodes from './instructionCodes'
 
-const moveOperation = (canvasContext2D, instructions, index, projection, cache) => {
-  const point = projection(cache[0], instructions.slice(index + 1, index + 3));
+const moveOperation = (canvasContext2D, instructions, index, cache) => {
+  const point = instructions.slice(index + 1, index + 3);
   canvasContext2D.moveTo(point[0], point[1]);
   return index + 3;
 };
 
-const lineOperation = (canvasContext2D, instructions, index, projection, cache) => {
-  const point = projection(cache[0], instructions.slice(index + 1, index + 3));
+const lineOperation = (canvasContext2D, instructions, index, cache) => {
+  const point = instructions.slice(index + 1, index + 3);
   canvasContext2D.lineTo(point[0], point[1]);
   return index + 3;
 };
 
-const quadOperation = (canvasContext2D, instructions, index, projection, cache) => {
-  const control = projection(
-    cache[0], instructions.slice(index + 1, index + 3));
-  const point = projection(
-    cache[1], instructions.slice(index + 3, index + 5));
+const quadOperation = (canvasContext2D, instructions, index, cache) => {
+  const control = instructions.slice(index + 1, index + 3);
+  const point = instructions.slice(index + 3, index + 5);
   canvasContext2D.quadraticCurveTo(
     control[0],
     control[1],
@@ -25,13 +23,10 @@ const quadOperation = (canvasContext2D, instructions, index, projection, cache) 
   return index + 5;
 };
 
-const bezierOperation = (canvasContext2D, instructions, index, projection, cache) => {
-  const controlA = projection(
-    cache[0], instructions.slice(index + 1, index + 3));
-  const controlB = projection(
-    cache[1], instructions.slice(index + 3, index + 5));
-  const point = projection(
-    cache[2], instructions.slice(index + 5, index + 7));
+const bezierOperation = (canvasContext2D, instructions, index, cache) => {
+  const controlA = instructions.slice(index + 1, index + 3);
+  const controlB = instructions.slice(index + 3, index + 5);
+  const point = instructions.slice(index + 5, index + 7);
   canvasContext2D.bezierCurveTo(
     controlA[0],
     controlA[1],
@@ -42,9 +37,9 @@ const bezierOperation = (canvasContext2D, instructions, index, projection, cache
   return index + 7;
 };
 
-const arcOperation = (canvasContext2D, instructions, index, projection, cache) => {
-  const point = projection(cache[0], instructions.slice(index + 1, index + 3));
-  const radius = projection(cache[1], [instructions[index + 3], 0])[0];
+const arcOperation = (canvasContext2D, instructions, index, cache) => {
+  const point = instructions.slice(index + 1, index + 3);
+  const radius = instructions[index + 3];
   const isCCW = instructions[index + 6] !== 0;
   canvasContext2D.arc(
     point[0],
@@ -64,7 +59,7 @@ renderOperations[instructionCodes.quad] = quadOperation;
 renderOperations[instructionCodes.bezier] = bezierOperation;
 renderOperations[instructionCodes.arc] = arcOperation;
 
-const performRender = (canvasContext2D, projection, cache) => instructions => {
+const performRender = (canvasContext2D, cache) => instructions => {
   const endIndex = instructions[0];
   let index = 1;
   while (index < endIndex) {
@@ -72,14 +67,13 @@ const performRender = (canvasContext2D, projection, cache) => instructions => {
       canvasContext2D,
       instructions,
       index,
-      projection,
       cache
     );
   }
   return instructions;
 };
 
-export const renderToCanvas = ({canvasContext2d, projection, instructions}) => {
+export const renderToCanvas = ({canvasContext2d, instructions}) => {
   const pointCacheBuffer = new ArrayBuffer(24);
   const cache = [
     new Float32Array(pointCacheBuffer, 0, 2),
@@ -87,7 +81,7 @@ export const renderToCanvas = ({canvasContext2d, projection, instructions}) => {
     new Float32Array(pointCacheBuffer, 4 * 4, 2)
   ];
 
-  const renderer = performRender(canvasContext2d, projection, cache);
+  const renderer = performRender(canvasContext2d, cache);
   if (instructions) {
     renderer(instructions);
     return renderer;
