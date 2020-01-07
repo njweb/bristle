ac477bec666737996e0c752e77480ff9ab24e800
nick jenkins <nickjenkinsweb@gmail.com>
2020-01-07 10:47:24 -0800

    update package config and build system

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,13 @@
 import babel from 'rollup-plugin-babel';
-import replace from 'rollup-plugin-replace';
+import replace from '@rollup/plugin-replace';
+import resolve from '@rollup/plugin-node-resolve';
 import {terser} from 'rollup-plugin-terser';
 
 const commonPlugins = [
   replace({
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
   }),
+  resolve(),
   babel({exclude: 'node_modules/**'})
 ];
 
@@ -19,7 +21,7 @@ const rollupConfig = [
     },
   },
   {
-    input: 'src/bristleUmd.js',
+    input: 'src/bristle.js',
     plugins: [...commonPlugins, terser()],
     output: {
       format: 'umd',
